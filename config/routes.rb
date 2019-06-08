6f736c044508c4ca7f0ebc6081df119aa26ac636
Umesh <ukk4732@gmail.com>
2019-06-08 10:02:40 +0530

    Added sign up api call for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do 
+      devise_scope :user do
+        post "sign_up", to: "registrations#create" 
+      end
+    end
+  end
+
 end
